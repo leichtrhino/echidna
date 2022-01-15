277e9b19917672bae777c09039f76d1ab49632da
leichtrhino <leichtrhino@outlook.jp>
2022-01-16 01:06:11 +0900

    Add test to check source_by_category parameter

--- tests/data/test_samples.py
+++ tests/data/test_samples.py
@@ -156,6 +156,72 @@ class TestSamples(unittest.TestCase):
         self.assertEqual(data['waves'].shape, (3, 12000))
         self.assertIn('sheets', data)
 
+    def test_sample_a3(self):
+        samplespec = self.samplespecs['A3']
+        samplespec.save_samples()
+        # check directory structure
+        metadata_path = self.sample_dir / 'a3' / 'metadata.json'
+        journal_path = self.sample_dir / 'a3' / 'journal.json'
+        sample_path = self.sample_dir / 'a3' / '000' / '000.pth'
+        self.assertTrue(os.path.isfile(metadata_path))
+        self.assertTrue(os.path.isfile(journal_path))
+        self.assertTrue(os.path.isfile(sample_path))
+
+        # load metadata
+        with open(metadata_path, 'r') as fp:
+            samples_ = samples.Sample.from_list(json.load(fp))
+        self.assertEqual(len(samples_), 1)
+        for s in samples_:
+            self.assertEqual(s.path, os.path.join('000', '000.pth'))
+            self.assertEqual(s.categories, ['ct001', 'ct001', 'ct002'])
+            self.assertEqual(s.tracks, [None, None, None])
+            self.assertEqual(s.folds, [None, None, None])
+            self.assertEqual(s.sample_rate, 8000)
+
+        # load journals
+        with open(journal_path, 'r') as fp:
+            journal = samples.SamplesJournal.from_dict(json.load(fp))
+        self.assertEqual(journal.metadata_path, 'metadata.json')
+        self.assertEqual(journal.spec.to_dict(), samplespec.to_dict())
+        self.assertEqual(journal.seed, self.seed)
+        self.assertEqual(len(journal.sample_journals), 1)
+        for j, s in zip(journal.sample_journals, samples_):
+            self.assertEqual(j.sample.to_dict(), s.to_dict())
+            self.assertEqual(j.length, 12000)
+            # find datasources from id
+            datasources = [
+                [
+                    next(e for e in journal.spec.datasources
+                         if e.id == dsid)
+                    for dsid in dsids
+                ]
+                for dsids in j.datasources
+            ]
+            self.assertEqual(
+                [dss[0].category for dss in datasources],
+                s.categories
+            )
+            self.assertEqual(
+                [dss[0].track for dss in datasources],
+                s.tracks
+            )
+            self.assertEqual(
+                [dss[0].fold for dss in datasources],
+                s.folds
+            )
+            self.assertEqual(
+                [len(dss) for dss in datasources],
+                [1 if c == 'ct001' else 2 if c == 'ct002' else None
+                 for c in s.categories]
+            )
+
+        # load data
+        data = torch.load(sample_path)
+        self.assertTrue(type(data), dict)
+        self.assertIn('waves', data)
+        self.assertEqual(data['waves'].shape, (3, 12000))
+        self.assertIn('sheets', data)
+
     def test_sample_b1(self):
         samplespec = self.samplespecs['B1']
         samplespec.save_samples()
