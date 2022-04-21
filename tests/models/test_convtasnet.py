230db5db46d34d75e2a97403365bdff42e870ad7
leichtrhino <leichtrhino@outlook.jp>
2022-04-22 01:32:09 +0900

    Add encoder/decoder test of ConvTasNet

--- tests/models/test_convtasnet.py
+++ tests/models/test_convtasnet.py
@@ -20,6 +20,103 @@ class TestConvTasNetModels(unittest.TestCase):
         self.assertEqual(block.reverse_length(20), sc.shape[-1])
         self.assertEqual(block.reverse_length(20), o.shape[-1])
 
+    def test_convtasnetencoder(self):
+        encoder = ctn.ConvTasNetEncoder(encoder_in_channel=1,
+                                        feature_channel=128,
+                                        block_channel=14,
+                                        bottleneck_channel=12,
+                                        skipconnection_channel=16,
+                                        kernel_size=9,
+                                        depth=3,
+                                        repeats=2)
+
+        target_length = 200
+        input_length = encoder.reverse_length(target_length)
+        output_length = encoder.forward_length(input_length)
+        channel = encoder.forward_channel()
+        self.assertEqual(channel, 16)
+
+        x = torch.rand(8, input_length)
+        embd, X = encoder(x)
+        self.assertEqual(embd.shape, (8, channel, output_length))
+        self.assertEqual(X.shape, (8, 1, 2, 128 // 2, output_length))
+
+    def test_convtasnetencoder_multiinput(self):
+        encoder = ctn.ConvTasNetEncoder(encoder_in_channel=2,
+                                        feature_channel=128,
+                                        block_channel=14,
+                                        bottleneck_channel=12,
+                                        skipconnection_channel=16,
+                                        kernel_size=9,
+                                        depth=3,
+                                        repeats=2)
+
+        target_length = 200
+        input_length = encoder.reverse_length(target_length)
+        output_length = encoder.forward_length(input_length)
+        channel = encoder.forward_channel()
+        self.assertEqual(channel, 16)
+
+        x = torch.rand(8, 2, input_length)
+        embd, X = encoder(x)
+        self.assertEqual(embd.shape, (8, channel, output_length))
+        self.assertEqual(X.shape, (8, 2, 2, 128 // 2, output_length))
+
+
+
+    def test_convtasnetdecoder(self):
+        encoder = ctn.ConvTasNetEncoder(encoder_in_channel=1,
+                                        feature_channel=128,
+                                        block_channel=14,
+                                        bottleneck_channel=12,
+                                        skipconnection_channel=16,
+                                        kernel_size=9,
+                                        depth=3,
+                                        repeats=2)
+        decoder = ctn.ConvTasNetDecoder(encoder_in_channel=1,
+                                        decoder_out_channel=2,
+                                        feature_channel=128,
+                                        skipconnection_channel=16)
+
+        target_length = 80
+        input_length = encoder.reverse_length(
+            decoder.reverse_length(target_length))
+        output_length = decoder.forward_length(
+            encoder.forward_length(input_length))
+
+        x = torch.rand(8, 1, input_length)
+        embd, X = encoder(x)
+        y = decoder(embd, X)
+        self.assertEqual(y.shape, (8, 2, output_length))
+
+    def test_convtasnetdecoder_multiinput(self):
+        encoder = ctn.ConvTasNetEncoder(encoder_in_channel=2,
+                                        feature_channel=128,
+                                        block_channel=14,
+                                        bottleneck_channel=12,
+                                        skipconnection_channel=16,
+                                        kernel_size=9,
+                                        depth=3,
+                                        repeats=2)
+        decoder = ctn.ConvTasNetDecoder(encoder_in_channel=2,
+                                        decoder_out_channel=3,
+                                        feature_channel=128,
+                                        skipconnection_channel=16)
+
+        target_length = 80
+        input_length = encoder.reverse_length(
+            decoder.reverse_length(target_length))
+        output_length = decoder.forward_length(
+            encoder.forward_length(input_length))
+
+        x = torch.rand(8, 2, input_length)
+        embd, X = encoder(x)
+        y = decoder(embd, X)
+        self.assertEqual(y.shape, (8, 3, 2, output_length))
+
+
+
+
     def test_conv_tasnet(self):
         ctasnet = ctn.ConvTasNet(in_channel=1,
                                  out_channel=2,
