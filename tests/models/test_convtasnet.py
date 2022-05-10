4a7597ea3ac5f5229024952206f567a554e47d5f
leichtrhino <leichtrhino@outlook.jp>
2022-05-11 00:52:23 +0900

    Rename test module for convtasnet

--- tests/models/test_convtasnet.py
+++ tests/models/test_convtasnet.py
@@ -1,35 +1,10 @@
+
 import unittest
-import math
-import itertools
 import torch
 
-import os, sys
-sys.path.append(os.path.abspath(os.path.join(__file__, '..', '..')))
-
-from chimerau.models import convtasnet as ctn
+from echidna.models import convtasnet as ctn
 
 class TestConvTasNetModels(unittest.TestCase):
-    def test_encoder(self):
-        encoder = ctn.TrainableStftLayer(512, 128)
-        x = torch.rand(8, 2, 16000)
-        X = encoder(x)
-        self.assertEqual(len(X.shape), 4)
-        self.assertEqual(X.shape[:-2], (8, 2))
-        self.assertEqual(X.shape[-2], 2*(512 // 2 + 1))
-        self.assertEqual(X.shape[-1], (16000 + 2 * 128*2 - 512) // 128 + 1)
-        self.assertEqual(encoder.forward_length(16000), X.shape[-1])
-        self.assertEqual(encoder.reverse_length(X.shape[-1]), 16000)
-
-    def test_decoder(self):
-        decoder = ctn.TrainableIstftLayer(512, 128)
-        X = torch.rand(8, 2, 512+2, 126)
-        x = decoder(X)
-        self.assertEqual(len(x.shape), 3)
-        self.assertEqual(x.shape[:-1], (8, 2))
-        self.assertEqual(x.shape[-1], 128 * (126-1) - 2*128*2 + 512)
-        self.assertEqual(decoder.forward_length(126), x.shape[-1])
-        self.assertEqual(decoder.reverse_length(x.shape[-1]), 126)
-
     def test_conv_block(self):
         block = ctn.ConvBlock(io_channel=14,
                               block_channel=12,
@@ -66,6 +41,7 @@ class TestConvTasNetModels(unittest.TestCase):
         self.assertEqual(signals.shape, (8, 2, 16000))
         self.assertEqual(embd.shape, (8, F, T))
 
+    def test_conv_tasnet_shared_masks(self):
         # test for shared masks
         ctasnet = ctn.ConvTasNet(in_channel=1,
                                  out_channel=2,
@@ -77,9 +53,11 @@ class TestConvTasNetModels(unittest.TestCase):
                                  depth=3,
                                  repeats=2,
                                  mask_each=False)
+        x = torch.rand(8, 1, 16000)
         signals = ctasnet(x)
         self.assertEqual(signals.shape, (8, 2, 16000))
 
+    def test_conv_tasnet_multi_input(self):
         # test for multi input
         ctasnet = ctn.ConvTasNet(in_channel=2,
                                  out_channel=3,
@@ -96,11 +74,35 @@ class TestConvTasNetModels(unittest.TestCase):
         self.assertEqual(signals.shape, (8, 3, 2, 16000))
 
         signals, embd = ctasnet(x, return_embd=True)
-        F = ctasnet.forward_embd_feature()
-        T = ctasnet.forward_embd_length(16000)
+        f = ctasnet.forward_embd_feature()
+        t = ctasnet.forward_embd_length(16000)
         self.assertEqual(signals.shape, (8, 3, 2, 16000))
-        self.assertEqual(embd.shape, (8, F, T))
+        self.assertEqual(embd.shape, (8, f, t))
 
+    def test_conv_tasnet_multi_input_shared_mask(self):
+        # test for multi input
+        ctasnet = ctn.ConvTasNet(in_channel=2,
+                                 out_channel=4,
+                                 feature_channel=130,
+                                 block_channel=14,
+                                 bottleneck_channel=12,
+                                 skipconnection_channel=16,
+                                 kernel_size=9,
+                                 depth=3,
+                                 repeats=2,
+                                 mask_each=False)
+
+        x = torch.rand(8, 2, 16000)
+        signals = ctasnet(x)
+        self.assertEqual(signals.shape, (8, 4, 2, 16000))
+
+        signals, embd = ctasnet(x, return_embd=True)
+        f = ctasnet.forward_embd_feature()
+        t = ctasnet.forward_embd_length(16000)
+        self.assertEqual(signals.shape, (8, 4, 2, 16000))
+        self.assertEqual(embd.shape, (8, f, t))
+
+    def test_conv_tasnet_codebook(self):
         # test for codebook>1
         ctasnet = ctn.ConvTasNet(in_channel=1,
                                  out_channel=2,
@@ -176,5 +178,3 @@ class TestConvTasNetModels(unittest.TestCase):
         T = cctasnet.forward_embd_length(16000)
         self.assertEqual(embd.shape, (8, F, T, 20))
 
-if __name__ == '__main__':
-    unittest.main()
