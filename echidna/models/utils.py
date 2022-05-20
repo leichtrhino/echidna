6f4d347ae5af149b46468ed10c51ce46cb3da023
leichtrhino <leichtrhino@outlook.jp>
2022-05-21 00:24:11 +0900

    Add initializers for CNN and LSTM

---
+++ echidna/models/utils.py
@@ -0,0 +1,37 @@
+
+from math import pi, sqrt, cos, sin
+import torch
+
+def init_conv_weight(conv : torch.nn.Conv1d) -> None:
+    torch.nn.init.xavier_normal_(conv.weight)
+    if conv.bias is not None:
+        fan_out, fan_in = \
+            torch.nn.init._calculate_fan_in_and_fan_out(conv.weight)
+        if (fan_in + fan_in) != 0:
+            std = sqrt(2 / (fan_in + fan_out))
+            torch.nn.init.normal_(conv.bias, std=std)
+
+def init_lstm_weight(lstm_layer : torch.nn.LSTM, hidden_size):
+    N = hidden_size
+    for name, param in lstm_layer.named_parameters():
+        N = hidden_size
+        if 'weight_ih' in name:
+            for i, gain in enumerate([
+                    torch.nn.init.calculate_gain('sigmoid'),
+                    torch.nn.init.calculate_gain('sigmoid'),
+                    torch.nn.init.calculate_gain('tanh'),
+                    torch.nn.init.calculate_gain('sigmoid')
+            ]):
+                torch.nn.init.xavier_uniform_(param[i*N:(i+1)*N], gain)
+        elif 'weight_hh' in name:
+            for i, gain in enumerate([
+                    torch.nn.init.calculate_gain('sigmoid'),
+                    torch.nn.init.calculate_gain('sigmoid'),
+                    torch.nn.init.calculate_gain('tanh'),
+                    torch.nn.init.calculate_gain('sigmoid')
+            ]):
+                torch.nn.init.orthogonal_(param[i*N:(i+1)*N], gain)
+        elif 'bias' in name:
+            param.data.fill_(0)
+            param.data[N:2*N].fill_(1) # for forget gate
+
