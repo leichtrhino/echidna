3242be352398566ab32fa5e61eca91d33ce8559b
leichtrhino <leichtrhino@outlook.jp>
2022-07-18 00:05:46 +0900

    Add mini scripts to work main script

---
+++ echidna/apps/separate.py
@@ -0,0 +1,14 @@
+
+import argparse
+
+def attach_parser(parser):
+    pass
+
+def main(args):
+    raise NotImplementedError()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('echidna-separate')
+    attach_parser(parser)
+    args = parser.parse_args()
+    main(args)
