05e3446169b2a51169a273445fd9c9e1d9cadfd4
leichtrhino <leichtrhino@outlook.jp>
2022-09-19 18:15:34 +0900

    Update dataset test

--- tests/data/test_datasets.py
+++ tests/data/test_datasets.py
@@ -1,15 +1,13 @@
 
 import unittest
 import os
+import json
 import pathlib
 import tempfile
 
-from echidna.data.samples import Sample
-from echidna.data.augmentations import Augmentation, AugmentationSpec
-from echidna.data.augmentations import RandomAugmentation
-from echidna.data.mixtures import Mixture, MixtureSpec
-from echidna.data.mixtures import CategoryMix
-from echidna.data.datasets import Dataset, BasicDataset, CompositeDataset
+from echidna.data.augmentations import AugmentationSpec
+from echidna.data.mixtures import MixSetSpec
+from echidna.data.datasets import Dataset
 
 from .utils import prepare_datasources
 
@@ -23,43 +21,46 @@ class TestDatasets(unittest.TestCase):
         self.samplespec = self.samplespecs['E1']
         self.samplespec.save_samples()
 
-        self.aug_dir = pathlib.Path(self.tmpdir.name) / 'aug'
         aug_spec = AugmentationSpec(
-            algorithm=RandomAugmentation(
-                source_sample_rate=8000,
-                target_sample_rate=8000,
-                waveform_length=int(8000 * 0.5),
+            source_sample_rate=8000,
+            target_sample_rate=8000,
+            waveform_length=int(8000 * 0.5),
 
-                normalize=True,
-                scale_range=[0.1, 1.1],
-                scale_point_range=[1, 5],
-                time_stretch_range=[0.5, 1.5],
-                pitch_shift_range=[0.5, 1.5],
-            ),
+            scale_range=[0.1, 1.1],
+            scale_point_range=[1, 5],
+            time_stretch_range=[0.5, 1.5],
+            pitch_shift_range=[0.5, 1.5],
             seed=self.seed,
-            augmentation_per_sample=2,
-            sample_metadata_path=self.sample_dir/'e1'/'metadata.json',
-            augmentation_metadata_path=self.aug_dir/'e1'/'metadata.json',
-            journal_path=self.aug_dir/'e1'/'journal.json',
-            log_path=self.aug_dir/'e1'/'log.txt',
-            log_level='DEBUG',
+            augmentation_per_parent=2,
+            input_metadata_path=self.sample_dir/'e1'/'metadata.json',
+            output_metadata_path=self.sample_dir/'e1'/'metadata_aug.json',
+            journal_path=self.sample_dir/'e1'/'journal_aug.json',
+            log_path=self.sample_dir/'e1'/'log_aug.txt',
+            log_level='INFO',
             jobs=None
         )
         aug_spec.save_augmentation()
 
-        self.mix_dir = pathlib.Path(self.tmpdir.name) / 'mix'
-        mix_spec = MixtureSpec(
-            algorithm=CategoryMix(
-                mix_category_list=[['ct001'], ['ct002', 'ct003']],
-                include_other=False,
-            ),
+        mix_spec = MixSetSpec(
+            mix_category_list=[
+                {
+                    'category': ['ct001'],
+                    'min_channel': 1,
+                    'max_channel': 3,
+                },
+                {
+                    'category': ['ct002', 'other'],
+                    'min_channel': 1,
+                    'max_channel': 3,
+                },
+            ],
             seed=self.seed,
-            mix_per_sample=2,
-            sample_metadata_path=self.sample_dir/'e1'/'metadata.json',
-            mixture_metadata_path=self.mix_dir/'e1'/'metadata.json',
-            journal_path=self.mix_dir/'e1'/'journal.json',
-            log_path=self.mix_dir/'e1'/'log.txt',
-            log_level='DEBUG',
+            mix_per_parent=2,
+            input_metadata_path=self.sample_dir/'e1'/'metadata_aug.json',
+            output_metadata_path=self.sample_dir/'e1'/'metadata_mix.json',
+            journal_path=self.sample_dir/'e1'/'journal_mix.json',
+            log_path=self.sample_dir/'e1'/'log_mix.txt',
+            log_level='INFO',
             jobs=None
         )
         mix_spec.save_mixture()
@@ -69,36 +70,12 @@ class TestDatasets(unittest.TestCase):
 
     def test_dataset(self):
         # initialize dataset
-        dataset = BasicDataset(
-            samples_metadata_path=self.sample_dir/'e1'/'metadata.json',
-            augmentations_metadata_path=self.aug_dir/'e1'/'metadata.json',
-            mixtures_metadata_path=self.mix_dir/'e1'/'metadata.json',
-        )
-
+        dataset = Dataset([self.sample_dir/'e1'/'metadata_mix.json'])
         self.assertTrue(len(dataset), 2 * 2 * 3)
-        for data, metadata in dataset:
-            waves = data['waves']
-            self.assertTrue(waves.shape, (2, int(8000 * 0.5)))
-
-        # serialize/deserialize
-        dataset_dict = dataset.to_dict()
-        dataset_from_dict = Dataset.from_dict(dataset_dict)
-        self.assertEqual(dataset.to_dict(), dataset_from_dict.to_dict())
-
-    def test_composite_dataset(self):
-        # initialize dataset
-        basic_dataset = BasicDataset(
-            samples_metadata_path=self.sample_dir/'e1'/'metadata.json',
-            augmentations_metadata_path=self.aug_dir/'e1'/'metadata.json',
-            mixtures_metadata_path=self.mix_dir/'e1'/'metadata.json',
-        )
-        dataset = CompositeDataset([basic_dataset, basic_dataset])
-
-        self.assertTrue(len(dataset), 2 * (2 * 2 * 3))
-        for i, (data, metadata) in enumerate(dataset):
-            waves = data['waves']
-            self.assertTrue(waves.shape, (2, int(8000 * 0.5)))
-            self.assertEqual(metadata['index'], i)
+        for data_list, metadata_list in dataset:
+            for data in data_list:
+                wave = data['wave']
+                self.assertTrue(wave.shape, (int(8000 * 0.5),))
 
         # serialize/deserialize
         dataset_dict = dataset.to_dict()
