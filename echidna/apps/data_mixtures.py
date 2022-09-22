db0fe71f0fbc2d11fbe78e2c661e5831456fb8ca
leichtrhino <leichtrhino@outlook.jp>
2022-09-22 18:32:11 +0900

    Update mixture app

--- echidna/apps/data_mixtures.py
+++ echidna/apps/data_mixtures.py
@@ -1,25 +1,38 @@
 
 import argparse
 
-from .utils import make_config_action, make_structure_action
-from ..data.mixtures import (
-    MixAlgorithm,
-    MixtureSpec,
-)
+from .utils import make_config_action, LoadJSONAction
+from ..data.mixtures import MixSetSpec
 
 def attach_parser(parser):
     config_schema = {
         'type': 'object',
         'properties': {
-            'algorithm': {'type': 'object'},
             'seed': {'type': 'integer'},
-            'mix_per_sample': {'type': 'integer'},
-            'sample_metadata_path': {'type': 'string'},
-            'mixture_metadata_path': {'type': 'string'},
+            'mix_category_list': {'type': 'array'},
+            'mix_per_parent': {
+                'Or': [
+                    {'type': 'integer'},
+                    {'type': 'null'},
+                ]
+            },
+            'input_metadata_path': {'type': 'string'},
+            'output_metadata_path': {'type': 'string'},
             'journal_path': {'type': 'string'},
             'log_path': {'type': 'string'},
             'log_level': {'type': 'string'},
-            'jobs': {'type': 'integer'},
+            'jobs': {
+                'Or': [
+                    {'type': 'integer'},
+                    {'type': 'null'},
+                ]
+            },
+            'device': {
+                'Or': [
+                    {'type': 'string'},
+                    {'type': 'null'},
+                ]
+            },
         },
         'additionalProperties': False,
     }
@@ -29,33 +42,34 @@ def attach_parser(parser):
                         action=make_config_action(config_schema))
 
     group = parser.add_argument_group(title='mixture parameters')
-    group.add_argument('--algorithm',
-                       action=make_structure_action(MixAlgorithm))
+    group.add_argument('--mix-category-list', action=LoadJSONAction)
     group.add_argument('--seed')
-    group.add_argument('--mix-per-sample')
+    group.add_argument('--mix-per-parent')
 
     group = parser.add_argument_group(title='output parameters')
-    group.add_argument('--sample-metadata-path')
-    group.add_argument('--mixture-metadata-path')
+    group.add_argument('--input-metadata-path')
+    group.add_argument('--output-metadata-path')
     group.add_argument('--journal-path')
 
     group = parser.add_argument_group(title='misc parameters')
     group.add_argument('--log-path')
     group.add_argument('--log-level')
     group.add_argument('--jobs')
+    group.add_argument('--device')
 
 
 def main(args):
-    spec = MixtureSpec(
-        algorithm=args.algorithm,
+    spec = MixSetSpec(
         seed=args.seed,
-        mix_per_sample=args.mix_per_sample,
-        sample_metadata_path=args.sample_metadata_path,
-        mixture_metadata_path=args.mixture_metadata_path,
+        mix_category_list=args.mix_category_list,
+        mix_per_parent=args.mix_per_parent,
+        input_metadata_path=args.input_metadata_path,
+        output_metadata_path=args.output_metadata_path,
         journal_path=args.journal_path,
         log_path=args.log_path,
         log_level=args.log_level,
         jobs=args.jobs,
+        device=args.device,
     )
     spec.save_mixture()
 
